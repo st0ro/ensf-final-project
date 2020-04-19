d95699fcdad8b8ff31e28d79d164177ee7cabfbc
CptWafflez <captainwafflez11@gmail.com>
2020-04-18 22:16:05 -0600

    Create IDBCredentials.java

---
+++ src/server/servermodel/IDBCredentials.java
@@ -0,0 +1,10 @@
+package server.servermodel;
+
+public interface IDBCredentials {
+	static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";  
+	static final String DB_URL = "jdbc:mysql://localhost/mydb";
+	
+	static final String USERNAME = "root";
+	static final String PASSWORD = "password";
+
+}
