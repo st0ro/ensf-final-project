27a81fc746037ce3b01c20118ddfa9b529108196
jameswzhou <56765078+jameswzhou@users.noreply.github.com>
2020-04-20 23:11:30 -0600

    Javadoc for IDBCredentials

--- src/server/servermodel/IDBCredentials.java
+++ src/server/servermodel/IDBCredentials.java
@@ -1,11 +1,29 @@
 package server.servermodel;
 
+/**
+ * An interface containing the credentials for logging into the database.
+ * @author James Zhou
+ *
+ */
 public interface IDBCredentials {
 	
+	/**
+	 * Java database connector driver location
+	 */
 	static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";  
+	/**
+	 * URL of the database
+	 */
 	static final String DB_URL = "jdbc:mysql://localhost/ensf409";
 	
+	/**
+	 * Database username
+	 */
 	static final String USERNAME = "root";
+	
+	/**
+	 * Database password
+	 */
 	static final String PASSWORD = "password";
 
 }
